d3f28e1c9ad61aa95474ecaff77c1e2664fbbb65
Welton Sanches <sanches.welton@gmail.com>
2024-10-27 18:18:50 -0300

    create initial style with daisyui components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function App() {
   return (
-    <h1>Let's start to play?</h1>
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className='container min-h-80 bg-violet-200 text-center align-middle rounded-lg'>
+        <h1 className="text-3xl text-center align-middle m-10	text-violet-950">
+          SIN Number Validator
+        </h1>
+
+        <input type="text" placeholder="SIN Number" className="input input-bordered input-primary w-full max-w-xs m-5" />
+        <button className="btn btn-primary">Validate</button>
+      </div>
+    </div>
   )
 }
 
